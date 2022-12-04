7fc461d4fa705567f9c70feb824c2b37d526ff4f
IvanKalug-QA <kaluginivan2002@mail.ru>
2022-12-04 18:33:18 +0300

    Added description of task

--- PostrgeSQL.js
+++ PostrgeSQL.js
@@ -1,3 +1,6 @@
 //Hello my name is Ivan
-
 //I'am learning QA Engineer
+
+//Task: Print the names of all the people who are in the airline database
+//Условие задачи: Выведите имена всех людей, которые есть в базе данных авиакомпании
+
