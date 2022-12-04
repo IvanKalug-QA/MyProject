f98e7e28ada5a2065800fdb79c050d06e88889fd
IvanKalug-QA <kaluginivan2002@mail.ru>
2022-12-04 18:34:12 +0300

    Complete task

--- PostrgeSQL.js
+++ PostrgeSQL.js
@@ -3,4 +3,5 @@
 
 //Task: Print the names of all the people who are in the airline database
 //Условие задачи: Выведите имена всех людей, которые есть в базе данных авиакомпании
-
+SELECT name
+FROM passenger;
