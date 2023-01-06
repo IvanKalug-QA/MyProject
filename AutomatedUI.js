42f347c7b2e8f1822ec4cdb5e4e783c0f770489b
IvanKalug-QA <kaluginivan2002@mail.ru>
2023-01-06 20:07:03 +0300

    Complete Autotest_2

--- AutomatedUI.js
+++ AutomatedUI.js
@@ -38,3 +38,40 @@ test();
 
 //My Autotest_2
 //Мой Автотест_2
+const puppeteer = require('puppeteer');
+const URL_TEST = 'https://www.codewars.com/';
+
+async function test() {
+  console.log('Запуск браузера');
+  const browser = await puppeteer.launch({headless: false, slowMo: 150, defaultViewport: null});
+
+  console.log('Создание новой вкладки в браузере');
+  const page = await browser.newPage();
+
+  console.log('Переход по ссылке');
+  await page.goto(URL_TEST);
+
+  console.log('Нажатия на поле SING IN');
+  const knock = await page.$('#login-btn');
+  await knock.click();
+
+  await page.waitForSelector('#users_sessions');
+
+  console.log('Ввод текста в поле login');
+  const elemeny = await page.$('#user_email');
+  await elemeny.type('You email');
+
+  console.log('Ввод текста в поле пароль');
+  const elemeny1 = await page.$('#user_password');
+  await elemeny1.type('You password')
+
+  console.log('Нажать на кнопку SIGN IN');
+  const  searh = await page.$('button[type="submit"]');
+  await searh.click();
+
+  await page.waitForSelector('#app');
+
+  console.log('Закрытие браузера');
+  await browser.close();
+}
+test();
